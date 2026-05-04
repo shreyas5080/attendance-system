3aac14cbcf60c37b85db6fd0f95755253d529e41
BroCoder <bro@example.com>
2026-05-04 19:39:25 +0530

    deploy ready

--- DataBase.py
+++ DataBase.py
@@ -5,10 +5,11 @@ import os
 load_dotenv()
 def init_database():
     conn = mysql.connector.connect(
-        host = "localhost",
-        port = 3306,
-        user = "root",
-        password = os.getenv("DB_PASSWORD"),
-        database = "my_database"
+        host="mysql-2b417a2d-shreyas5080.l.aivencloud.com",
+        port=24706,  
+        user="avnadmin",
+        password=os.getenv("DB_PASSWORD"),
+        database="my_database",
+        ssl_ca="C:/Users/Shreyas/Downloads/ca (1).pem"
     )
     return conn
